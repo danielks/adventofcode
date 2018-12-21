78f93b55eb848e81e3bb23353e7c319208b3117b
Daniel <daniel@bludata.com.br>
2018-12-20 22:42:48 -0200

    Dia 5 otimizado.
     Antes: 100s. Agora: 0,22s.

--- day5.py
+++ day5.py
@@ -1,3 +1,5 @@
+#otimizado. levava uns 100 segundos para rodar, agora leva aprox 0,22s
+
 conteudo = 'dabAcCaCBAcCcaDA'
 
 with open("day5input.txt", "r") as f:
@@ -8,25 +10,27 @@ conteudo = conteudo_bruto[0]
 def polaridades_reversas(c1, c2):
     return c1.upper() == c2.upper() and ord(c1) != ord(c2)
 
+import time
+start_time = time.time()
 
 continua = True
-while continua:
-    for i in range(len(conteudo)):
-        c = conteudo[i]
 
-        if i < len(conteudo)-1:
-            seguinte = conteudo[i+1]
+i = 0
 
-            if polaridades_reversas(c, seguinte):
-                conteudo = conteudo[:i] + conteudo[i+2:]
-                break
-        else:
-            continua = False
+while i < len(conteudo)-1:
+    c = conteudo[i]
 
+    seguinte = conteudo[i+1]
 
-            
+    if polaridades_reversas(c, seguinte):
+        conteudo = conteudo[:i] + conteudo[i+2:]
 
-
-        #print(c)
+        if i > 0:
+            i = i - 1
+    else:
+        i += 1
 
 print(len(conteudo))
+print(conteudo)
+
+print("Esse processo levou %s segundos" % (time.time() - start_time))
